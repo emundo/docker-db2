f684c1ac1c52c0b24a664ff047a648532b784931
David Bimamisa <info@shaka-b.de>
2018-04-17 14:22:19 +0200

    Use env variable for db2 user password

--- init_db2_tables.sh
+++ init_db2_tables.sh
@@ -7,7 +7,7 @@ db2start
 
 db2 create database rcitest
 
-db2 connect to rcitest USER db2inst1 USING dockertester
+db2 connect to rcitest USER db2inst1 USING ${DB2INST1_PASSWORD}
 
 db2 -tvf /db2.ddl
 db2 terminate
