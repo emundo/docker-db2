fe99d596a1e44b8d4ae800c659bb7b3ea39614d4
David Bimamisa <info@shaka-b.de>
2018-04-18 16:42:21 +0200

    Update init_db2_tables.sh

--- init_db2_tables.sh
+++ init_db2_tables.sh
@@ -4,16 +4,12 @@ set -e
 su - db2inst1 << EOF
 whoami
 db2start
-
-#db2set DB2COMM=tcpip
+db2set DB2COMM=tcpip
 
 db2 create database rcitest
 
 db2 connect to rcitest USER db2inst1 USING dockertester
 #${DB2INST1_PASSWORD}
 
-db2 -tvf /db2.ddl
-#db2 terminate
-#db2stop
 EOF
 whoami
